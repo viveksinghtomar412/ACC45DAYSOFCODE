9807852c26b835fb95172215e46683661456c8c2
viveksinghtomar412 <115340819+viveksinghtomar412@users.noreply.github.com>
2022-10-29 20:17:13 +0530

    Add files via upload

---
+++ divideprogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("enter any number");
+    scanf("%d",&num);
+    if((num%5==0) && (num%11==0))
+    {
+        printf("number is divisible by 5 and 11");
+    }
+    else
+    {
+        printf("number is not divisible by 5 and 11");
+    }
+    return 0;
+}
