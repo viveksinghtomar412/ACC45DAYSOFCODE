3acd12a8136f23edf313eba45e7d9509f6989a0e
viveksinghtomar412 <115340819+viveksinghtomar412@users.noreply.github.com>
2022-11-01 22:42:50 +0530

    Add files via upload

---
+++ greaterno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("enter a no.");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("a is greater");
+    }
+    else
+    {
+        printf("b is greater");
+    }
+    return 0;
+}
