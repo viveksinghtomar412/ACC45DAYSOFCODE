3d2da46da35de124d346f47d5724cb47956bce83
viveksinghtomar412 <115340819+viveksinghtomar412@users.noreply.github.com>
2022-11-17 23:41:07 +0530

    Add files via upload

---
+++ string 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+ int main()
+ {
+    char firt[]="",second[30]="";
+    int i=0,len; 
+    printf("enter first string\n");
+    gets(first);
+    for(i=0;i<=len;i++)
+    {
+        second[i]='\0'
+        printf("concatination of both string is %s ",second);
+        retrun 0
+    }
+ }
