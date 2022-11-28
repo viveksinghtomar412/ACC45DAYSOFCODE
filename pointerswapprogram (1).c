70eab454eb4a245749033bbf8dc6b61eebef2eca
viveksinghtomar412 <115340819+viveksinghtomar412@users.noreply.github.com>
2022-11-28 23:39:12 +0530

    Add files via upload

---
+++ pointerswapprogram (1).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int swap(int *, int *);
+int main(){
+    int a,b;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    swap(&a,&b);
+    printf("a=%d and b=%d",a,b);
+    return 0;
+}
+int swap(int *p, int *q)
+{
+    int temp;
+    temp=*p;
+    *p=*q;
+    *q=temp;
+}
